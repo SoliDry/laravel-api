4e7da4f9e9150858a2fb543806ee996d6b3b977c
zeusakm <zeusakm@gmail.com>
2016-12-20 08:49:37 +0300

    *	hot-fix. routes json api specs

--- Modules/V2/Http/routes.php
+++ Modules/V2/Http/routes.php
@@ -1,9 +1,9 @@
 <?php
 Route::group(['middleware' => 'web', 'prefix' => 'v2', 'namespace' => 'Modules\V2\Http\Controllers'], function()
 {
-    Route::get('/article/index', 'ArticleController@index');
-    Route::get('/article/view/{id}', 'ArticleController@view');
-    Route::post('/article/create', 'ArticleController@create');
-    Route::patch('/article/update/{id}', 'ArticleController@update');
-    Route::delete('/article/delete/{id}', 'ArticleController@delete');
+    Route::get('/article', 'ArticleController@index');
+    Route::get('/article/{id}', 'ArticleController@view');
+    Route::post('/article', 'ArticleController@create');
+    Route::patch('/article/{id}', 'ArticleController@update');
+    Route::delete('/article/{id}', 'ArticleController@delete');
 });
