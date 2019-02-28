5ae79384d6dfcf2051d9e8ef4682b7430b5821ae
akushman <zeusakm@gmail.com>
2019-02-28 22:26:18 +0300

    #144. App generated files

---
+++ app/Entities/Topic.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Entities;
+
+use SoliDry\Extension\BaseModel;
+
+class Topic extends BaseModel 
+{
+    // >>>props>>>
+    protected $primaryKey = 'id';
+    protected $table = 'topic';
+    public $timestamps = false;
+    // <<<props<<<
+    // >>>methods>>>
+
+    public function article() 
+    {
+        return $this->hasMany(Article::class);
+    }
+    // <<<methods<<<
+}
---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Http\Controllers;
+
+class MenuController extends DefaultController 
+{
+    // >>>props>>>
+    // <<<props<<<
+}
